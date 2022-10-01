36cd1932012e265e41c707cfce95da6a42b70c2c
NEVA <neva.vka@yandex.ru>
2022-10-01 18:54:39 +0300

    Первое задание

--- Ex01/Program.cs
+++ Ex01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Максимальное число {a}");
+}
+else Console.WriteLine($"Максимальное число {b}");
