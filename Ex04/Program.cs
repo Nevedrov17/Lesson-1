18caa8ac94b849044ef3894a271650b142c9e73f
NEVA <neva.vka@yandex.ru>
2022-10-01 19:31:06 +0300

    Четвертое задание

---
+++ Ex04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 1;
+while (a < n)
+{
+    if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+    a++;
+}
