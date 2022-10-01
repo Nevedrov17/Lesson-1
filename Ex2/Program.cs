9f9f7c1135f5ec3f6f2f7dfee0c65637dac4a228
NEVA <neva.vka@yandex.ru>
2022-10-01 19:12:13 +0300

    Второе задание

---
+++ Ex2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write($"Максимальное число {a}");
+}
+else if (b > c)
+{
+    Console.Write($"Максимальное число {b}");
+}
+else Console.Write($"Максимальное число {c}");
