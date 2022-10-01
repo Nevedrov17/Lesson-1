da8a2a76ee4b6d04132d98f09cadf78eee901e82
NEVA <neva.vka@yandex.ru>
2022-10-01 19:22:26 +0300

    третье задание

---
+++ Ex03/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"{a} четное число");
+}
+else Console.WriteLine($"{a} нечетное число");
